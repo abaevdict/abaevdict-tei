8fea3763f26354537cd7653a118425300b0180c2
Oleg Belyaev <obelyaev@gmail.com>
2021-04-02 13:31:28 +0300

    changed algorithm to replacement, primitive but works

--- ossetic-sort.py
+++ ossetic-sort.py
@@ -2,7 +2,7 @@
 
 import sys
 
-lines = sys.stdin.read().strip().split('\n')
+lines = sys.stdin.read().strip().splitlines()
 
 abaev_alphabet = {
     'a': '0', 'A': '0',
@@ -15,13 +15,18 @@ abaev_alphabet = {
     'e': '7', 'E': '7',
     'f': '8', 'F': '8',
     'g': '9', 'G': '9',
+    'g0': '9', 'G0': '9',
     'ǵ': '0', 'Ǵ': '0',
     'ǧ': 'A', 'Ǧ': 'A',
+    'ǧ0': 'A', 'Ǧ0': 'A',
     'i': 'B', 'I': 'B',
     'ī': 'C', 'Ī': 'C',
     'j': 'D', 'J': 'D',
     'k': 'E', 'K': 'E',
+    'k0': 'E', 'K0': 'E',
     'ḱ': 'F', 'Ḱ': 'F',
+    'kʼ': 'E', 'Kʼ': 'E',
+    'kʼ0': 'E', 'K0ʼ': 'E',    
     'l': 'G', 'L': 'G',
     'm': 'H', 'M': 'H',
     'n': 'I', 'N': 'I',
@@ -29,6 +34,7 @@ abaev_alphabet = {
     'p': 'K', 'P': 'K',
     'pʼ': 'L', 'Pʼ': 'L',
     'q': 'M', 'Q': 'M',
+    'q0': 'M', 'Q0': 'M',
     'r': 'N', 'R': 'N',
     's': 'O', 'S': 'O',
     't': 'P', 'T': 'P',
@@ -37,6 +43,7 @@ abaev_alphabet = {
     'v': 'S', 'V': 'S',
     'w': 'T', 'W': 'T',
     'x': 'U', 'X': 'U',
+    'x0': 'U', 'X0': 'U',
     'y': 'V', 'Y': 'V',
     'z': 'W', 'Z': 'W'
         }
@@ -44,9 +51,139 @@ abaev_alphabet = {
 def abaev_key(x):
     x = x.replace('entries/abaev_','')
     #print (x)
-    if x[0] == 'ᵆ':
+    if x[0] == 'ᵆ' or x[0] == '-' or x[0] =='*':
         x = x[1:]
-    return ''.join(abaev_alphabet.get(ch, ch) for ch in x)
+    x = x.replace('a','/')
+    x = x.replace('A', '/')
+    
+    x = x.replace('æ', '1')
+    x = x.replace('Æ', '1')
+    
+    x = x.replace('b', '2')
+    x = x.replace('B', '2')
+    
+    x = x.replace('cʼ', '4')
+    x = x.replace('Cʼ', '4')
+    
+    x = x.replace('c', '3')
+    x = x.replace('C', '3')
+    
+    x = x.replace('d', '5')
+    x = x.replace('D', '5')
+    
+    x = x.replace('ʒ', '6')
+    x = x.replace('Ʒ', '6')
+    
+    x = x.replace('e', '7')
+    x = x.replace('E', '7')
+    
+    x = x.replace('f', '8')
+    x = x.replace('F', '8')
+
+    x = x.replace('g0', '9')
+    x = x.replace('G0', '9')
+    
+    x = x.replace('g', '9')
+    x = x.replace('G', '9')
+    
+    x = x.replace('ǵ', '9')
+    x = x.replace('Ǵ', '9')
+
+    x = x.replace('ǧ0', 'A')
+    x = x.replace('Ǧ0', 'A')
+    
+    x = x.replace('ǧ', 'A')
+    x = x.replace('Ǧ', 'A')
+    
+    x = x.replace('i', 'B')
+    x = x.replace('I', 'B')
+    
+    x = x.replace('ī', 'B')
+    x = x.replace('Ī', 'B')
+    
+    x = x.replace('j', 'D')
+    x = x.replace('J', 'D')
+
+    x = x.replace('kʼ0', 'F')
+    x = x.replace('Kʼ0ʼ', 'F')
+
+    x = x.replace('kʼ', 'F')
+    x = x.replace('Kʼ', 'F')
+
+    x = x.replace('k0', 'E')
+    x = x.replace('K0', 'E')
+    
+    x = x.replace('k', 'E')
+    x = x.replace('K', 'E')
+
+    x = x.replace('ḱʼ', 'F')
+    x = x.replace('Ḱʼ', 'F')
+    
+    x = x.replace('ḱ', 'E')
+    x = x.replace('Ḱ', 'E')
+
+    x = x.replace('l', 'H')
+    x = x.replace('L', 'H')
+    
+    x = x.replace('m', 'I')
+    x = x.replace('M', 'I')
+    
+    x = x.replace('n', 'J')
+    x = x.replace('N', 'J')
+    
+    x = x.replace('o', 'K')
+    x = x.replace('O', 'K')
+    
+    x = x.replace('pʼ', 'M')
+    x = x.replace('Pʼ', 'M')    
+    
+    x = x.replace('p', 'L')
+    x = x.replace('P', 'L')
+
+    x = x.replace('q0', 'N')
+    x = x.replace('Q0', 'N')
+    
+    x = x.replace('q', 'N')
+    x = x.replace('Q', 'N')
+
+    x = x.replace('r', 'O')
+    x = x.replace('R', 'O')
+    
+    x = x.replace('s', 'P')
+    x = x.replace('S', 'P')
+
+    x = x.replace('tʼ', 'R')
+    x = x.replace('Tʼ', 'R')
+    
+    x = x.replace('t', 'Q')
+    x = x.replace('T', 'Q')
+    
+    x = x.replace('u', 'S')
+    x = x.replace('U', 'S')
+    
+    x = x.replace('ū', 'S')
+    x = x.replace('Ū', 'S')
+    
+    x = x.replace('v', 'T')
+    x = x.replace('V', 'T')
+    
+    x = x.replace('w', 'U')
+    x = x.replace('W', 'U')
+
+    x = x.replace('x0', 'V')
+    x = x.replace('X0', 'V')
+    
+    x = x.replace('x', 'V')
+    x = x.replace('X', 'V')
+    
+    x = x.replace('y', 'W')
+    x = x.replace('Y', 'W')
+    
+    x = x.replace('z', 'X')
+    x = x.replace('Z', 'X')
+    
+    #return ''.join(abaev_alphabet.get(ch, ch) for ch in x)
+    return x
 
 lines.sort(key = abaev_key)
 print("\n".join(lines))
